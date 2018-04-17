fe8ef0c23ec53a1a8b5479fcb8c339c575c5bd3e
Katharine McLeod <katharinermcleod@gmail.com>
2018-04-17 16:49:14 -0400

    simple database

---
+++ scratch/queries.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const data = require('../db/notes');
+const simDB = require('../db/simDB');
+const notes = simDB.initialize(data);
+
+// GET Notes with search
+notes.filter('cats', (err, list) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log(list);
+});
+
+// GET Notes by ID
+notes.find(20000, (err, item) => {
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
+
+// PUT (Update) Notes by ID
+const updateObj = {
+  title: 'New Title',
+  content: 'Blah blah blah'
+};
+
+notes.update(20000, updateObj, (err, item) => {
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 'use strict';
 
 const express = require('express');
+
+
+// Simple In-Memory Database
 const data = require('./db/notes');
+const simDB = require('./db/simDB');  // <<== add this
+const notes = simDB.initialize(data); // <<== and this
+
 const { PORT } = require('./config');
 const {middleLogger} =require('./middleware/logger.js');
 const app = express();
@@ -14,13 +20,26 @@ app.use(middleLogger);
 
 
 
-app.get('/api/notes', (req, res) => {
-  let searchInput = req.query.searchTerm;
-
-  //MENTOR SESSION: WHY DON'T I NEED TO RETURN?
-  (!searchInput) ? res.json(data) : res.json(data.filter(items => items.title.includes(searchInput)));
-
+// app.get('/api/notes', (req, res) => {
+ 
+app.get('/api/notes', (req, res, next) => {
+  const { searchTerm } = req.query;
+  notes.filter(searchTerm, (err, list) => {
+    if (err) {
+      return next(err); // goes to error handler
+    }
+    res.json(list); // responds with filtered array
+  });
 });
+
+
+ 
+// let searchInput = req.query.searchTerm;
+
+//MENTOR SESSION: WHY DON'T I NEED TO RETURN?
+// (!searchInput) ? res.json(data) : res.json(data.filter(items => items.title.includes(searchInput)));
+
+// });
 
 app.get('/api/notes/:id', (req, res) => {
   const id = req.params.id;
@@ -30,11 +49,22 @@ app.get('/api/notes/:id', (req, res) => {
 
 });
 
+
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   res.status(404).json({ message: 'Not Found' });
 });
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+
 
 // Listen for incoming connections
 app.listen(PORT, function () {
